44429857b4d899b57e7c1706286597ce2e03ee56
Daniel Einy <daniel.einy@gmail.com>
2021-11-16 18:09:11 +0200

    python data utils

---
+++ python_code/data_utils/utils.py
@@ -0,0 +1,42 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+plt.style.use('Solarize_Light2')
+
+
+def plot_hist(value_counts, file='plot.png', title=[], inches=(16, 9)):
+    plt.clf()
+    plt.figure(figsize=inches)
+    if title:
+        plt.title(title)
+    value_counts.plot(kind='bar')
+    plt.savefig(file)
+
+def mismatch_positions(seq_a, seq_b):
+    assert len(seq_a) == len(seq_b)
+    filter_a = (seq_a != '.') & (seq_a != 'N')
+    filter_b = (seq_b != '.') & (seq_b != 'N')
+    compare = seq_a != seq_b
+    return np.where(filter_a & filter_b & compare)[0]
+
+def load_multiple_sets(db_paths: list, columns: list):
+    sets = [pd.read_csv(x, sep='\t', usecols=columns) for x in db_paths]
+    return pd.concat(sets)
+
+def clone_size_distribution(list_of_sets, 
+                            clone_column='clone_id', 
+                            min_dupcount=0, 
+                            min_conscount=0):
+    stacked = pd.Series()
+
+    for set_path in list_of_sets:
+       repertoire = pd.read_csv(set_path, sep='\t')
+       # TODO: filter function
+       repertoire = repertoire[(~repertoire.clone_id.isna()) & \
+                               (~repertoire.sequence_id.str.contains('FAKE')) & \
+                               (repertoire.consensus_count >= min_conscount) & \
+                               (repertoire.duplicate_count >= min_dupcount)]
+       clone_sizes = repertoire.clone_id.value_counts()
+       stacked =  pd.concat([stacked, clone_sizes])
+
+    return stacked.value_counts()
