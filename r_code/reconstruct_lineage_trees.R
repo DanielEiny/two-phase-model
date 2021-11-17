9864da8ff16ebc18f4c7003aac0284d704243bdd
Daniel Einy <daniel.einy@gmail.com>
2021-11-17 19:47:59 +0200

    mutations from lineage trees: working!

--- r_code/reconstruct_lineage_trees.R
+++ r_code/reconstruct_lineage_trees.R
@@ -6,6 +6,7 @@ library(stringr)
 
 reconstruct_lineage_trees <- function(data_set_path) {
     # ----- Read & filter data ----- #
+    # TODO: drop irrelevant columns
     # TODO: filter by conscount, dupcount - use cli arguments
     repertoire <- read.table(data_set_path,
                              sep = "\t",
@@ -36,5 +37,62 @@ reconstruct_lineage_trees <- function(data_set_path) {
                      buildPhylipLineage,
                      phylip_exec = phylip_exec,
                      rm_temp = TRUE)
-    return(graphs)
+    graphs[sapply(graphs, is.null)] <- NULL  # In case of singleton clone
+
+    # ----- Add new columns ----- #
+    repertoire$ancestor_alignment <- repertoire$germline_alignment
+    repertoire$sequence_origin <- "OBSERVED"
+    repertoire$ancestor_origin <- "GERMLINE"
+
+    # ----- Loop over lineage graphs, fill columns and append rows ----- #
+    for (g in graphs){
+            edges <- get.edgelist(g)
+            vertex_attributes <- get.vertex.attribute(g)
+
+            clone_representative_id <- vertex_attributes$label[which(!grepl("Inferred|Germline", vertex_attributes$label))[1]]
+            clone_representative <- repertoire[which(repertoire$sequence_id == clone_representative_id), ]
+            
+            for (i in 1:dim(edges)[1]){
+                    descendant_id <- edges[i, 2]
+                    ancestor_id <- edges[i, 1]
+                    ancestor_alignment <- vertex_attributes$sequence[which(vertex_attributes$label == ancestor_id)]
+
+                    # Fill ancestor sequence / add entire row
+                    observed <- !grepl("Inferred", descendant_id)
+
+                    if (observed) {  # Observed sequence, row already exist
+                            descendant_loc <- which(repertoire$sequence_id == descendant_id)
+                            repertoire$ancestor_alignment[descendant_loc] <- ancestor_alignment 
+
+                    } else {  # Inferred sequence, add row
+                            new_row <- repertoire[1, ]
+                            new_row[] <- NA
+                            new_row$sequence_origin <- "PHYLOGENY_INFERRED"
+                            new_row$sequence_alignment <- vertex_attributes$sequence[i]
+                            new_row$ancestor_alignment <- ancestor_alignment
+                            new_row$v_call <- clone_representative$v_call
+                            new_row$d_call <- clone_representative$d_call
+                            new_row$j_call <- clone_representative$j_call
+                            new_row$junction_length <- clone_representative$junction_length
+
+                            descendant_loc <- nrow(repertoire) + 1
+                            repertoire <- rbind(repertoire, new_row)
+                    }
+
+                    # Fill ancestor origin
+                    if (ancestor_id == "Germline") {
+                            ancestor_origin <- "Germline"
+                    } else {
+                            if (observed) {
+                                    ancestor_origin <- "PHYLOGENY_OBSERVED"
+                            } else {
+                                    ancestor_origin <- "PHYLOGENY_INFERRED"
+                            }
+                    }
+                    repertoire$ancestor_origin[descendant_loc] <- ancestor_origin
+            }
+
+    }
+
+    return(repertoire)
 }
