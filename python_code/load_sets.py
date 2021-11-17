efad4146fdea8e4749d5008518a8a0e1d45515a1
Daniel Einy <daniel.einy@gmail.com>
2021-11-17 11:31:45 +0200

    typo

--- python_code/load_sets.py
+++ python_code/load_sets.py
@@ -1,4 +1,4 @@
- --- Single sample ---
+# --- Single sample --- #
 HCV = pd.read_csv('/work/peresay/vdjbase/V9/P4/P4_I1_S1/P4_I1_S1.tsv', sep='\t')
 INF = pd.read_csv('/work/peresay/vdjbase/V9/P3/P3_I1_S1/P3_I1_S1.tsv', sep='\t')
 MS = pd.read_csv('/work/peresay/vdjbase/V9/P10/P10_I2_S1/P10_I2_S1.tsv', sep='\t')
@@ -7,7 +7,7 @@ CANCER = pd.read_csv('/work/data/colon_cancer/100794_H/100794_H_cloned_w_filtere
 
 
 
- --- Complete data sets ---
+# --- Complete data sets --- #
 hcv_sets = glob.glob('/work/peresay/vdjbase/V9/P4/*/*S1.tsv')
 inf_sets = glob.glob('/work/peresay/vdjbase/V9/P3/*/*S1.tsv')
 ms_sets = glob.glob('/work/peresay/vdjbase/V9/P10/*/*S1.tsv')
